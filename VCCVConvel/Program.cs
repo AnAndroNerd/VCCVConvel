c71ae398d3d9fda4f8b957df8441d33e33689ee4
Anjo <87346264+AnAndroNerd@users.noreply.github.com>
2025-07-23 17:56:23 -0700

    Add project files.

---
+++ VCCVConvel/Program.cs
@@ -0,0 +1,28 @@
+﻿using UtauPlugin;
+
+namespace sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UtauPlugin.UtauPlugin utauPlugin = new UtauPlugin.UtauPlugin(args[0]);
+            utauPlugin.Input();
+            foreach (Note note in utauPlugin.note)
+            {
+                float baseConvel = 100 * (note.GetTempo() / 120);
+
+                if (note.GetLength() >= 480)
+                {
+                    note.SetVelocity((int)(baseConvel + (100 - (100 * (note.GetLength() / 480)))));
+                }
+                if (note.GetLength() < 480)
+                {
+                    note.SetVelocity((int)(baseConvel + (50 + (-100 * (note.GetLength() / 960)))));
+                }
+
+                utauPlugin.Output();
+            }
+        }
+    }
+}

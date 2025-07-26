299ba3801c849c20442baa084ab4ca172c507bbd
Anjo <87346264+AnAndroNerd@users.noreply.github.com>
2025-07-25 21:34:21 -0700

    makes the thing work

--- VCCVConvel/Program.cs
+++ VCCVConvel/Program.cs
@@ -1,24 +1,32 @@
-﻿using UtauPlugin;
+﻿using System;
+using UtauPlugin;
+
+
 
 namespace sample1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
             UtauPlugin.UtauPlugin utauPlugin = new UtauPlugin.UtauPlugin(args[0]);
             utauPlugin.Input();
             foreach (Note note in utauPlugin.note)
             {
+
                 float baseConvel = 100 * (note.GetTempo() / 120);
 
                 if (note.GetLength() >= 480)
                 {
-                    note.SetVelocity((int)(baseConvel + (100 - (100 * (note.GetLength() / 480)))));
+                    int Velocity = (int)(baseConvel + (50 - 100 * ((float)note.GetLength() / 960)));
+                    note.SetVelocity(Velocity);
                 }
                 if (note.GetLength() < 480)
                 {
-                    note.SetVelocity((int)(baseConvel + (50 + (-100 * (note.GetLength() / 960)))));
+                    int Velocity = (int)(baseConvel + (100 - (100 * ((float)note.GetLength() / 480))));
+                    note.SetVelocity(Velocity);
                 }
             }
 
